729c4c5b4bd738518a5c9d90108934229b09e189
Wedge Taylor <wtaylor@et-j5.kzn.21-school.ru>
2021-11-21 16:54:12 +0300

    finish

--- srcs/requirements/Wordpress/tools/setup.sh
+++ srcs/requirements/Wordpress/tools/setup.sh
@@ -1,5 +1,5 @@
 #!bin/sh
 wp core download --allow-root --path=/var/www/wordpress
 wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --skip-check --allow-root
-wp config set WP_HOME http://$DOMAIN_NAME --raw --allow-root
-wp config set WP_SITEURL http://$DOMAIN_NAME --raw --allow-root
+wp config set WP_HOME \'http://$DOMAIN_NAME\' --raw --allow-root
+wp config set WP_SITEURL \'http://$DOMAIN_NAME\' --raw --allow-root
